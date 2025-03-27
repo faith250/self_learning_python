742f299c5d1f57e28afa260b283a0a9d562c451a
faith250 <aasthakkataria@gmail.com>
2025-03-27 09:30:36 +0530

    module 6 complete

---
+++ imp.py
@@ -0,0 +1,8 @@
+from fibo import fib
+print(fib(500))
+import(dir(fibo))
+
+>>> import fibo
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+>>> dir(fibo)
+['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'fib']
